9b59c147bbe0c3ffb423b156d7c604d4c0f8eb08
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2017-01-08 22:46:37 +0200

    Add files via upload

---
+++ LockOnPlugin/LockOnBehaviourMaker.cs
@@ -0,0 +1,20 @@
+﻿using IllusionPlugin;
+using System;
+using UnityEngine;
+
+namespace LockOnPlugin
+{
+    class LockOnBehaviourMaker : MonoBehaviour
+    {
+        void Start()
+        {
+            Console.WriteLine("Script \"{0}\" started", GetType().Name);
+            FindObjectOfType<CameraControl>().isOutsideTargetTex = !ModPrefs.GetBool("LockOnPlugin", "HideCameraTarget", true, true);
+        }
+
+        void OnDestroy()
+        {
+            Console.WriteLine("Script \"{0}\" destroyed", GetType().Name);
+        }
+    }
+}
---
+++ LockOnStudioPlugin/LockOnStudioPlugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using IllusionPlugin;
+using UnityEngine;
+
+namespace LockOnStudioPlugin
+{
+    public class LockOnStudioPlugin : IEnhancedPlugin
+    {
+        public string Name => GetType().Name;
+        public string Version => "1.0.0";
+
+        public string[] Filter => new string[]
+        {
+            "HoneyStudio_32",
+            "HoneyStudio_64"
+        };
+
+        public void OnApplicationStart()
+        {
+            Console.Write("{0} ", Name);
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+            if(level == 1 && !GameObject.Find("LockOnStudioBehaviour"))
+                new GameObject("LockOnStudioBehaviour").AddComponent<LockOnStudioBehaviour>();
+        }
+
+        public void OnUpdate(){}
+        public void OnLateUpdate(){}
+        public void OnApplicationQuit(){}
+        public void OnLevelWasInitialized(int level){}
+        public void OnFixedUpdate(){}
+    }
+}

aa26da0c9038eb500c972dcfb307d6f4e50d1be5
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2017-06-17 15:01:30 +0300

    v2.0.0
    NEO

---
+++ LockOnNeoPlugin/NeoProperties.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace LockOnPlugin
+{
+    internal partial class NeoMono : LockOnBase
+    {
+        protected override float CameraMoveSpeed
+        {
+            get { return camera.moveSpeed; }
+            set { camera.moveSpeed = value; }
+        }
+
+        protected override Vector3 CameraTargetPos
+        {
+            get { return camera.targetPos; }
+            set { camera.targetPos = value; }
+        }
+
+        protected override Vector3 LockOnTargetPos
+        {
+            get { return lockOnTarget.transform.position; }
+        }
+
+        protected override Vector3 CameraAngle
+        {
+            get { return camera.cameraAngle; }
+            set { camera.cameraAngle = value; }
+        }
+
+        protected override float CameraFov
+        {
+            get { return Camera.main.fieldOfView; }
+            set { Camera.main.fieldOfView = value; }
+        }
+
+        protected override Vector3 CameraDir
+        {
+            get { return cameraData.distance; }
+            set { cameraData.distance = value; }
+        }
+
+        protected override bool CameraTargetTex
+        {
+            set { camera.isConfigTargetTex = value; }
+        }
+    }
+}
---
+++ LockOnPluginParty/LockOnPlugin.cs
@@ -0,0 +1,34 @@
+﻿using IllusionPlugin;
+using UnityEngine;
+using LockOnPluginUtilities;
+
+namespace LockOnPlugin
+{
+    public class LockOnPlugin : IEnhancedPlugin
+    {
+        public string Name => GetType().Name;
+        public string Version => "2.0.0";
+
+        public string[] Filter => new string[]
+        {
+            "HoneySelect_32",
+            "HoneySelect_64",
+        };
+
+        public void OnLevelWasLoaded(int level)
+        {
+            //if(level == 15 && !GameObject.Find("HSceneMono") && FileManager.TargetSettingsExist())
+            //    new GameObject("HSceneMono").AddComponent<HSceneMono>();
+
+            if(level == 21 && !GameObject.Find("MakerMono") && FileManager.TargetSettingsExist())
+                new GameObject("MakerMono").AddComponent<MakerMono>();
+        }
+
+        public void OnUpdate(){}
+        public void OnLateUpdate(){}
+        public void OnApplicationStart(){}
+        public void OnApplicationQuit(){}
+        public void OnLevelWasInitialized(int level){}
+        public void OnFixedUpdate(){}
+    }
+}

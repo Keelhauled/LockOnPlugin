bd05e464da3d0c55dba6b987bffc4d9c1584e3fc
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2017-07-24 07:05:57 +0300

    Fixes with harmony

--- LockOnNeoPlugin/NeoMono.cs
+++ LockOnNeoPlugin/NeoMono.cs
@@ -17,6 +17,7 @@ namespace LockOnPlugin
         private TreeNodeCtrl treeNodeCtrl => studio.treeNodeCtrl;
 
         private Studio.CameraControl.CameraData cameraData;
+        private Studio.CameraControl.CameraData cameraReset;
         private OCIChar currentCharaOCI;
 
         protected override void Start()
@@ -24,6 +25,7 @@ namespace LockOnPlugin
             base.Start();
             
             cameraData = GetSecureField<Studio.CameraControl.CameraData, Studio.CameraControl>("cameraData", camera);
+            cameraReset = GetSecureField<Studio.CameraControl.CameraData, Studio.CameraControl>("cameraReset", camera);
             treeNodeCtrl.onSelect += new Action<TreeNodeObject>(OnSelectWork);
             studio.onDelete += new Action<ObjectCtrlInfo>(OnDeleteWork);
             Transform systemMenuContent = studio.transform.Find("Canvas Main Menu/04_System/Viewport/Content");
@@ -31,6 +33,7 @@ namespace LockOnPlugin
             systemMenuContent.Find("End").GetComponent<Button>().onClick.AddListener(() => showLockOnTargets = false);
             InstallNearClipPlaneSlider();
             StartCoroutine(InstallSettingsReloadButton());
+            NeoPatches.Init();
         }
 
         protected override void LoadSettings()
@@ -43,6 +46,13 @@ namespace LockOnPlugin
             Camera.main.nearClipPlane = nearClipPlane;
             GameObject nearClipSlider = GameObject.Find("Slider NearClipPlane");
             if(nearClipSlider) nearClipSlider.GetComponent<Slider>().value = nearClipPlane;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            InputKeyProc();
         }
 
         private void OnSelectWork(TreeNodeObject node)
@@ -293,5 +303,167 @@ namespace LockOnPlugin
                 Console.WriteLine("NearClipPlane text installed");
             }
         }
+
+        protected virtual bool InputKeyProc()
+        {
+            bool flag = false;
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                camera.Reset(0);
+            }
+            else if(Input.GetKeyDown(KeyCode.Keypad5))
+            {
+                cameraData.rotate.x = cameraReset.rotate.x;
+                cameraData.rotate.y = cameraReset.rotate.y;
+            }
+            else if(Input.GetKeyDown(KeyCode.Slash))
+            {
+                cameraData.rotate.z = 0f;
+            }
+            else if(Input.GetKeyDown(KeyCode.Semicolon))
+            {
+                camera.fieldOfView = cameraReset.parse;
+            }
+            float deltaTime = Time.deltaTime;
+            if(Input.GetKey(KeyCode.Home))
+            {
+                flag = true;
+                cameraData.distance.z = cameraData.distance.z + deltaTime;
+                cameraData.distance.z = Mathf.Min(0f, cameraData.distance.z);
+            }
+            else if(Input.GetKey(KeyCode.End))
+            {
+                flag = true;
+                cameraData.distance.z = cameraData.distance.z - deltaTime;
+            }
+
+            if(!lockOnTarget)
+            {
+                if(Input.GetKey(KeyCode.RightArrow))
+                {
+                    flag = true;
+                    if(camera.transBase != null)
+                    {
+                        cameraData.pos += camera.transBase.InverseTransformDirection(Camera.main.transform.TransformDirection(new Vector3(deltaTime, 0f, 0f)));
+                    }
+                    else
+                    {
+                        cameraData.pos += Camera.main.transform.TransformDirection(new Vector3(deltaTime, 0f, 0f));
+                    }
+                }
+                else if(Input.GetKey(KeyCode.LeftArrow))
+                {
+                    flag = true;
+                    if(camera.transBase != null)
+                    {
+                        cameraData.pos += camera.transBase.InverseTransformDirection(Camera.main.transform.TransformDirection(new Vector3(-deltaTime, 0f, 0f)));
+                    }
+                    else
+                    {
+                        cameraData.pos += Camera.main.transform.TransformDirection(new Vector3(-deltaTime, 0f, 0f));
+                    }
+                }
+                if(Input.GetKey(KeyCode.UpArrow))
+                {
+                    flag = true;
+                    if(camera.transBase != null)
+                    {
+                        cameraData.pos += camera.transBase.InverseTransformDirection(Camera.main.transform.TransformDirection(new Vector3(0f, 0f, deltaTime)));
+                    }
+                    else
+                    {
+                        cameraData.pos += Camera.main.transform.TransformDirection(new Vector3(0f, 0f, deltaTime));
+                    }
+                }
+                else if(Input.GetKey(KeyCode.DownArrow))
+                {
+                    flag = true;
+                    if(camera.transBase != null)
+                    {
+                        cameraData.pos += camera.transBase.InverseTransformDirection(Camera.main.transform.TransformDirection(new Vector3(0f, 0f, -deltaTime)));
+                    }
+                    else
+                    {
+                        cameraData.pos += Camera.main.transform.TransformDirection(new Vector3(0f, 0f, -deltaTime));
+                    }
+                }
+                if(Input.GetKey(KeyCode.PageUp))
+                {
+                    flag = true;
+                    cameraData.pos.y = cameraData.pos.y + deltaTime;
+                }
+                else if(Input.GetKey(KeyCode.PageDown))
+                {
+                    flag = true;
+                    cameraData.pos.y = cameraData.pos.y - deltaTime;
+                }
+            }
+
+            float num = 10f * Time.deltaTime;
+            Vector3 zero = Vector3.zero;
+            if(Input.GetKey(KeyCode.Period))
+            {
+                flag = true;
+                zero.z += num;
+            }
+            else if(Input.GetKey(KeyCode.Backslash))
+            {
+                flag = true;
+                zero.z -= num;
+            }
+            if(Input.GetKey(KeyCode.Keypad2))
+            {
+                flag = true;
+                zero.x -= num * camera.yRotSpeed;
+            }
+            else if(Input.GetKey(KeyCode.Keypad8))
+            {
+                flag = true;
+                zero.x += num * camera.yRotSpeed;
+            }
+            if(Input.GetKey(KeyCode.Keypad4))
+            {
+                flag = true;
+                zero.y += num * camera.xRotSpeed;
+            }
+            else if(Input.GetKey(KeyCode.Keypad6))
+            {
+                flag = true;
+                zero.y -= num * camera.xRotSpeed;
+            }
+            if(flag)
+            {
+                cameraData.rotate.y = (cameraData.rotate.y + zero.y) % 360f;
+                cameraData.rotate.x = (cameraData.rotate.x + zero.x) % 360f;
+                cameraData.rotate.z = (cameraData.rotate.z + zero.z) % 360f;
+            }
+            float deltaTime2 = Time.deltaTime;
+            if(Input.GetKey(KeyCode.Equals))
+            {
+                flag = true;
+                camera.fieldOfView = Mathf.Max(cameraData.parse - deltaTime2 * 15f, 1f);
+            }
+            else if(Input.GetKey(KeyCode.RightBracket))
+            {
+                flag = true;
+                camera.fieldOfView = Mathf.Min(cameraData.parse + deltaTime2 * 15f, camera.limitFov);
+            }
+            return flag;
+        }
+
+        //public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        //{
+        //    //var codes = new List<CodeInstruction>();
+        //    var ilinstructions = MethodBodyReader.GetInstructions(typeof(LockOnBase).GetMethod("TestProper", BindingFlags.NonPublic | BindingFlags.Instance));
+        //    //var ilinstructions = MethodBodyReader.GetInstructions(typeof(LockOnBase).GetMethod("TestReplacer"));
+        //    //for(int i = 0; i < ilinstructions.Count; i++)
+        //    //{
+        //    //    codes.Add(ilinstructions[i].GetCodeInstruction());
+        //    //}
+        //    var codetrans = new CodeTranspiler(ilinstructions);
+        //    var results = codetrans.GetResult(generator, typeof(LockOnBase).GetMethod("TestProper", BindingFlags.NonPublic | BindingFlags.Instance));
+        //    return results;
+        //    //return codes.AsEnumerable();
+        //}
     }
 }
